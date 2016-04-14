83bc4657af7cf4a6109284253c053cc51927eef5
higorfigueira <figueirahigor@gmail.com>
2016-04-13 22:08:05 -0300

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -5,9 +5,35 @@
 using namespace std;
 
 int main(){
-  Pessoa *pessoa;
-  pessoa = new Pessoa();
-  
+  int idade;
+  Pessoa *pessoa, *pessoa2, *pessoa3, *pessoa4, *pessoa5, *raiz = NULL;
+  pessoa = new Pessoa(20);
+  pessoa2 = new Pessoa (22);
+  pessoa3 = new Pessoa (18);
+  pessoa4 = new Pessoa (16);
+  pessoa5 = new Pessoa (24);
+
+  raiz = pessoa;
+  raiz->inserir(pessoa2);
+  raiz->inserir(pessoa3);
+  raiz->inserir(pessoa4);
+  raiz->inserir(pessoa5);
+
+  //cout << raiz->getDireita()->getId() << endl;
+
+
+  cout << "informe a idade que deseja buscar: " << endl;
+  cin >> idade;
+
+  //raiz->busca(idade);
+
+  raiz->mostra();
+
+  // delete pessoa;
+  // delete pessoa2;
+  // delete pessoa3;
+  // delete pessoa4;
+  // delete pessoa5;
 
   return 0;
 };
--- pessoa.hpp
+++ pessoa.hpp
@@ -1,21 +1,34 @@
+using namespace std;
+// int inserir(Pessoa *novo);
+//bool busca (int idade);
+// void mostra ();
+
 class Pessoa{
+
+
+
   protected:
     int id;
-    Pessoa *direita, *esquerda;
+    Pessoa *direita, *esquerda, *pai;
 
   public:
+
+    //Pessoa* busca (int idade);
+
     Pessoa(){
       id = 0;
       direita = esquerda = NULL;
     };
 
-    Pessoa(int id, Pessoa *direta, Pessoa *direita){
-      id = id;
+    Pessoa(int id){
+      this->id = id;
       direita = NULL;
       esquerda = NULL;
+      pai = NULL;
     };
 
     ~Pessoa(){
+
     };
 
     //Gets e Sets
@@ -25,27 +38,40 @@ class Pessoa{
 
     void setId(int id){
       id=id;
-    }
+    };
     Pessoa *getDireita (){
       return direita;
     };
 
     void setDireita(Pessoa *direita){
       direita=direita;
-    }
+    };
+
     Pessoa *getEsquerda (){
       return esquerda;
     };
 
     void setEsquerda(Pessoa *esquerda){
       esquerda=esquerda;
-    }
+    };
+
+    Pessoa *getPai (){
+      return pai;
+    };
+
+    void setPai(Pessoa *pai){
+      pai=pai;
+    };
     //FIM GET e SET
 
 
-    /*int inserir(Pessoa *novo){
+    int inserir(Pessoa *novo){
+      // if (/* condition */) {
+      //   /* code */
+      // }
       if(novo->id >= this->id){
               if(this->direita == NULL){
+                novo->pai = this;
                 this->direita = novo;
                 return 1;
               }
@@ -54,6 +80,7 @@ class Pessoa{
       }
       else{
           if(this->esquerda == NULL){
+            novo->pai = this;
             this->esquerda = novo;
             return 1;
           }
@@ -62,7 +89,98 @@ class Pessoa{
         }
 
     }
-*/
 
+
+    //função bucar PESSOA
+
+    Pessoa* busca (int idade){
+      if(idade == this->id) {
+        cout << "encontrado" << endl;
+        return this;
+      }
+      else {
+        if(this->id < idade && this->direita!=NULL){
+          return this->direita->busca(idade);
+        }
+        else
+          if(this->esquerda != NULL)
+            return this->esquerda->busca(idade);
+      }
+
+      cout << "Não encontrado " << this->id << endl;
+      return NULL;
+    };
+
+    void mostra (){
+      cout << this->getId() << endl;
+      if(this->esquerda == NULL && this->esquerda == NULL)
+        return;
+
+      else{
+        if(this->esquerda!=NULL)
+          this->esquerda->mostra();
+        if(this->direita!=NULL)
+          this->direita->mostra();
+      }
+    };
+
+
+
+    //função DELETAR PESSOA
+    bool deletar (int idade){
+      Pessoa *apagar = this->busca(idade);
+      if(apagar->getPai() == NULL && apagar->getDireita() == NULL && apagar->getEsquerda() == NULL){
+        delete apagar;
+        return true;
+      }
+
+      if (apagar->getDireita() != NULL && apagar->getEsquerda() == NULL && apagar->getPai() != NULL){
+          apagar->pai->setDireita(apagar->getDireita());
+          apagar->direita->setPai(apagar->getPai());
+          delete apagar;
+          return true;
+      }
+
+      if (apagar->getEsquerda() != NULL && apagar->getDireita() == NULL && apagar->getPai() != NULL){
+        apagar->pai->setEsquerda(apagar->getEsquerda());
+        apagar->esquerda->setPai(apagar->getPai());
+        delete apagar;
+        return true;
+      }
+
+    };
+
+
+
+
+
+
+
+
+
+    //   if(this->id == idade){
+    //     if (this->pai == NULL && this->direita==NULL && this->esquerda==NULL)
+    //       delete this;
+    //
+    //     if (this->id < idade){
+    //       if(this->direita->id == idade){
+    //         this->direita->direita->pai = this;
+    //         this->direita = this->direita->direita;
+    //         delete this;
+    //       }
+    //       else
+    //         return this->deletar(idade);
+    //     }
+    //     if(this->id > idade){
+    //       if(this->esquerda->id == idade){
+    //         this->esquerda->esquerda->pai = this;
+    //         this->esquerda = this->esquerda->esquerda;
+    //         delete this;
+    //       }
+    //       else
+    //         return this->deletar(idade)
+    //     }
+    //
+    //   }
 
 };
